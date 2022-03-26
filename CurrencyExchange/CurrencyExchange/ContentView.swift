cbee183406a9a76f4d9e754fb8c16e2c20657de5
Semaj Andrews <semajzandrews@gmail.com>
2022-03-26 17:08:39 -0400

    First Commit. Creating CurrencyExchange Application

---
+++ CurrencyExchange/CurrencyExchange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CurrencyExchange
+//
+//  Created by Semaj Andrews on 3/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CurrencyExchange/CurrencyExchange/CurrencyExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeApp.swift
+//  CurrencyExchange
+//
+//  Created by Semaj Andrews on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
